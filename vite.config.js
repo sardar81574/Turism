2404496c09c25a09093e6f2b39b927d1739af8f3
sardar dhakad <sardardhakad81@gamil.com>
2026-01-13 00:18:23 +0530

    Fix MIME error and Netlify build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: "/",   // 👈 IMPORTANT for Netlify
-})
+});
