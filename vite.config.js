33619541e26d63a5a37520e649895fcc72c25ebf
sardar dhakad <sardardhakad81@gamil.com>
2026-01-12 23:56:34 +0530

    updated the pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
-  
-   
-  
+  base: "/",   // 👈 IMPORTANT for Netlify
 })
